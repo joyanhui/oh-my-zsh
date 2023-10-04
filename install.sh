98348431d058dcb00812461878911468589ebd44
joyanhui <40133116+joyanhui@users.noreply.github.com>
2023-10-04 10:27:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd
-rm -rf .oh-my-zsh && rm main.zip &&rm -rf .zshrc
+cd ～
+rm -rf .oh-my-zsh && rm -r main.zip .zshrc
 wget https://github.com/joyanhui/oh-my-zsh/archive/refs/heads/main.zip -O main.zip
-
 unzip main.zip && mv oh-my-zsh-main/.oh-my-zsh  . 
-cp oh-my-zsh-main/.zshrc .&& rm main.zip
+cp oh-my-zsh-main/.zshrc .
+rm -rf main.zip .zshrc oh-my-zsh-main
