787154b800e9976b05d63d8b48afd83d607053ab
joyanhui <40133116+joyanhui@users.noreply.github.com>
2023-11-19 17:24:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 cd ～
 rm -rf .oh-my-zsh && rm -rf  main.zip .zshrc
-wget http://ghproxy.com/https://github.com/joyanhui/oh-my-zsh/archive/refs/heads/main.zip -O main.zip
+wget http://mirror.ghproxy.com/https://github.com/joyanhui/oh-my-zsh/archive/refs/heads/main.zip -O main.zip
 unzip main.zip 
 mv oh-my-zsh-main/.oh-my-zsh  . && cp oh-my-zsh-main/.zshrc .
 rm -rf main.zip oh-my-zsh-main
