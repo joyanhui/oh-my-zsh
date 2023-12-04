75b3f8f6ba85282fdad97e123a01bb725a2444c6
joyanhui <40133116+joyanhui@users.noreply.github.com>
2023-12-04 09:34:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~
-rm -rf .oh-my-zsh && rm -rf  yhZsh.zip .zshrc
+rm -rf .oh-my-zsh yhZsh.zip .zshrc
 curl -o yhZsh.zip ${GhProxy}https://github.com/joyanhui/oh-my-zsh/archive/refs/heads/main.zip
 unzip yhZsh.zip 
 mv oh-my-zsh-main/.oh-my-zsh  . && cp oh-my-zsh-main/.zshrc .
