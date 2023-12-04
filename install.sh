6b1565fcb8c78c91fae54c7313e504d52216bb6f
joyanhui <40133116+joyanhui@users.noreply.github.com>
2023-12-04 09:19:08 +0800

    echo zsh >> .bashrc

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cd ～
-rm -rf .oh-my-zsh && rm -rf  main.zip .zshrc
-wget http://mirror.ghproxy.com/https://github.com/joyanhui/oh-my-zsh/archive/refs/heads/main.zip -O main.zip
-unzip main.zip 
+rm -rf .oh-my-zsh && rm -rf  yhZsh.zip .zshrc
+wget http://mirror.ghproxy.com/https://github.com/joyanhui/oh-my-zsh/archive/refs/heads/main.zip -O yhZsh.zip
+unzip yhZsh.zip 
 mv oh-my-zsh-main/.oh-my-zsh  . && cp oh-my-zsh-main/.zshrc .
-rm -rf main.zip oh-my-zsh-main
+rm -rf yhZsh.zip oh-my-zsh-main
+
+#echo zsh >> .bashrc
